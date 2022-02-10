34ffd9062c0ace8a6225ccfa55a318a9f2de53f8
skhin <88125976+skhin@users.noreply.github.com>
2022-02-10 17:53:19 +0800

    deployment path

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ require("./config/db");
 
 const cors = require("cors");
 
+const path = require("path");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 const authRoutes = require("./routes/authRoute");
 const restaurantRoutes = require("./routes/restaurantRoutes");
 const userSettingRoutes = require("./routes/userRoutes");
@@ -22,6 +27,13 @@ app.use("/api", authRoutes);
 app.use("/api", restaurantRoutes);
 app.use("/api", userSettingRoutes);
 
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
+});
+
 //  ************** LISTENER ************** //
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+///////////////////////////////////////////
