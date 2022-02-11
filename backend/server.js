e7f15183babb649c91a3a0c6a8cca8bfd728bea8
skhin <88125976+skhin@users.noreply.github.com>
2022-02-11 21:29:17 +0800

    heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,6 @@ const cors = require("cors");
 
 const path = require("path");
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
 const authRoutes = require("./routes/authRoute");
 const restaurantRoutes = require("./routes/restaurantRoutes");
 const userSettingRoutes = require("./routes/userRoutes");
@@ -27,10 +24,16 @@ app.use("/api", authRoutes);
 app.use("/api", restaurantRoutes);
 app.use("/api", userSettingRoutes);
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
-});
+// For HEROKU deployment
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  // Index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 //  ************** LISTENER ************** //
 const PORT = process.env.PORT || 5001;
